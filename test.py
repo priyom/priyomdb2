74d0494475f04cc6539518f5e9a6f8b6e6c1701a
Jonas Wielicki <j.wielicki@sotecware.net>
2014-03-27 23:07:46 +0100

    Use mysqlconnector (python3 compatibility)

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 import priyom.consistency
 import priyom.model as model
 
-engine = create_engine('mysql://priyom2@localhost/priyom2', echo=False)
+engine = create_engine('mysql+mysqlconnector://priyom2@localhost/priyom2', echo=False)
 
 model.Base.metadata.create_all(engine)
 session = sessionmaker(bind=engine)()
