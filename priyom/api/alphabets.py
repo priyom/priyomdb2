3be649487fcca6ba954f7b4e852e3be03d231ffb
Jonas Wielicki <j.wielicki@sotecware.net>
2014-04-10 13:34:09 +0200

    Improve alphabet editing workflow (same as for modulation)

--- priyom/api/alphabets.py
+++ priyom/api/alphabets.py
@@ -1,5 +1,7 @@
 from sqlalchemy import func
+import sqlalchemy.exc
 
+import teapot.forms
 import teapot.request
 
 import priyom.model
@@ -46,45 +48,125 @@ def view_alphabets(request: teapot.request.Request, view):
         "alphabets": alphabets,
         "view": view,
         "view_alphabets": view_alphabets,
+        "edit_alphabet": edit_alphabet,
+        "delete_alphabet": delete_alphabet,
         "form": AlphabetForm()
     }, {}
 
 @require_capability("admin")
-@dbview(priyom.model.Alphabet,
-        [
-            ("id", priyom.model.Alphabet.id, None),
-            ("short_name", priyom.model.Alphabet.short_name, None),
-            ("display_name", priyom.model.Alphabet.display_name, None),
-            ("user_count", subquery(
-                priyom.model.TransmissionContents,
-                func.count('*').label('user_count')
-            ).group_by(
-                priyom.model.TransmissionContents.alphabet_id), int)
-        ],
-        itemsperpage=25,
-        default_orderfield="display_name")
-@router.route("/alphabet", order=0, methods={teapot.request.Method.POST})
-@xsltea_site.with_template("view_alphabets.xml")
-def view_alphabets_POST(request: teapot.request.Request, view):
+@router.route("/alphabet/{alphabet_id:d}/edit",
+              order=0,
+              methods={teapot.request.Method.GET})
+@xsltea_site.with_template("alphabet_form.xml")
+def edit_alphabet(request: teapot.request.Request, alphabet_id=0):
+    form = AlphabetForm()
+    existing = request.dbsession.query(
+        priyom.model.Alphabet
+    ).get(alphabet_id)
+    if existing is not None:
+        form.short_name = existing.short_name
+        form.display_name = existing.display_name
+
+
+    yield teapot.response.Response(None)
+    yield {
+        "alphabet_id": alphabet_id,
+        "form": form
+    }, {}
+
+@require_capability("admin")
+@router.route("/alphabet/{alphabet_id:d}/edit",
+              order=0,
+              methods={teapot.request.Method.POST})
+@xsltea_site.with_template("alphabet_form.xml")
+def edit_alphabet_POST(request: teapot.request.Request, alphabet_id=0):
     form = AlphabetForm(request=request)
+
     if not form.errors:
         dbsession = request.dbsession
-        new_alphabet = priyom.model.Alphabet(
-            form.short_name,
-            form.display_name)
-        dbsession.add(new_alphabet)
-        dbsession.commit()
-        raise teapot.make_redirect_response(
-            request,
-            view_alphabets,
-            view=view)
+        existing = dbsession.query(
+            priyom.model.Alphabet
+        ).get(alphabet_id)
+        try:
+            if existing is None:
+                existing = priyom.model.Alphabet(
+                    form.short_name,
+                    form.display_name)
+            else:
+                existing.short_name = form.short_name
+                existing.display_name = form.display_name
+            dbsession.add(existing)
+            dbsession.commit()
+        except sqlalchemy.exc.IntegrityError:
+            teapot.forms.ValidationError(
+                "Short name and display name must be unique",
+                None,
+                form).register()
+            dbsession.rollback()
+        else:
+            raise teapot.make_redirect_response(
+                request,
+                view_alphabets)
 
-    alphabets = list(page)
     yield teapot.response.Response(None)
 
     yield {
-        "alphabets": alphabets,
-        "page": page,
-        "view_alphabets": view_alphabets,
+        "alphabet_id": alphabet_id,
         "form": form
+    }, {}
+
+
+@require_capability("admin")
+@router.route("/alphabet/{alphabet_id:d}/delete",
+              order=0,
+              methods={teapot.request.Method.GET})
+@xsltea_site.with_template("alphabet_delete.xml")
+def delete_alphabet(request: teapot.request.Request,
+                    alphabet_id=0):
+    form = teapot.forms.Form()
+
+    yield teapot.response.Response(None)
+    yield {
+        "form": form,
+        "alphabet_id": alphabet_id
+    }, {}
+
+@require_capability("admin")
+@router.route("/alphabet/{alphabet_id:d}/delete",
+              order=0,
+              methods={teapot.request.Method.POST})
+@xsltea_site.with_template("alphabet_delete.xml")
+def delete_alphabet_POST(request: teapot.request.Request,
+                         alphabet_id=0):
+    form = teapot.forms.Form()
+
+    dbsession = request.dbsession
+    existing = dbsession.query(
+        priyom.model.Alphabet
+    ).get(alphabet_id)
+    if existing is not None:
+        try:
+            dbsession.delete(existing)
+            dbsession.commit()
+        except sqlalchemy.exc.IntegrityError:
+            dbsession.rollback()
+            teapot.forms.ValidationError(
+                "This alphabet is still in use",
+                None,
+                form).register()
+        else:
+            raise teapot.response.make_redirect_response(
+                request,
+                view_alphabets)
+
+    else:
+        teapot.forms.ValidationError(
+            "This alphabet does not exist",
+            None,
+            form).register()
+
+    yield teapot.response.Response(None)
+    yield {
+        "form": form,
+        "alphabet_id": alphabet_id
     }, {}
