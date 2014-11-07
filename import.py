6b9732dba054fd7dc2bd959c79be26225d8ac93f
Jonas Wielicki <j.wielicki@sotecware.net>
2014-11-07 18:29:15 +0100

    Improve import script

--- import.py
+++ import.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
+import logging
 import os
-import re
 import pickle
+import re
+import sys
 
 sys.path.insert(0, os.path.abspath("../teapot"))
 
@@ -29,8 +30,19 @@ _dbengine = sqlalchemy.create_engine(
     convert_unicode=True)
 dbsession = sqlalchemy.orm.sessionmaker(bind=_dbengine)()
 
+logger = logging.getLogger()
+
 E07_FORMAT = re.compile(r"\d{3}\s+\d{1,3}")
 E07A_FORMAT = re.compile(r"\d{3}\s+\d{1,3}\s+\d{5}")
+
+class F:
+    def __init__(self, fmt, *args, **kwargs):
+        self.fmt = fmt
+        self.args = args
+        self.kwargs = kwargs
+
+    def __str__(self):
+        return self.fmt.format(*self.args, **self.kwargs)
 
 def prefilter_contents_text(s):
     return s.replace("&nbsp;", " ").replace("&nbsp", " ")
@@ -59,8 +71,7 @@ def convert_station(node):
     station.status = node.find(pxmlns.Status).text
     station.location = node.find(pxmlns.Location).text
     dbsession.commit()
-    print("mapped station: {} -> {}".format(
-        id, station.id))
+    logger.info(F("mapped station: {} -> {}", id, station.id))
 
 def convert_mode(display_name):
     try:
@@ -119,8 +130,7 @@ def convert_broadcast(node):
         dbsession.commit()
 
     broadcast_map[id] = broadcast.id
-    print("mapped broadcast: {} -> {}".format(
-        id, broadcast.id))
+    logger.info(F("mapped broadcast: {} -> {}", id, broadcast.id))
 
 CONTENTS_SPLIT_RE = re.compile(
     "(([^/ ]+)|([^/ ]+)/([^/ ]+):(\w+))( |$)")
@@ -180,8 +190,10 @@ def convert_transmission(node):
         try:
             format_handler = format_handler_map[int(node.find(pxmlns.ClassID).text)]
         except KeyError as err:
-            print("could not convert transmission {}: missing format map entry for {}".format(
-                node.find(pxmlns.ID).text,
+            logger.error(F(
+                "cannot convert transmission (txid={}): "
+                "missing format map entry for format id {}",
+                id,
                 err))
             return
 
@@ -246,6 +258,13 @@ def simple_format_handler(format_id):
             if not callsign_node.text:
                 continue
             alphabet = callsign_node.get("lang", "la")
+            if alphabet == "la":
+                try:
+                    callsigns_per_alphabet[alphabet] = callsign_map[callsign_node.text]
+                except KeyError:
+                    pass
+                else:
+                    continue
             callsigns_per_alphabet[alphabet] = callsign_node.text
 
         try:
@@ -276,18 +295,28 @@ def fmt_5figure_groups(with_callsign, without_callsign, e07a_format, e07_format)
     e07 = simple_format_handler(e07_format)
     e07a = simple_format_handler(e07a_format)
     def handle_5figure_format(broadcast, contents, node):
+        logger.debug("5fig disambiguator")
         callsign_node = node.find(pxmlns.Callsign)
         if callsign_node is not None and callsign_node.text:
-            callsign_text = callsign_node.text
+            callsign_text = callsign_node.text.replace("/", " ")
+            if callsign_text.strip() == contents[0].strip():
+                logger.debug("callsign matches message, stripping")
+                callsign_node.text = None
+                return without_callsign(broadcast, contents, node)
+            callsign_node.text = callsign_text
             if E07A_FORMAT.match(callsign_text):
+                logger.debug("using e07a")
                 return e07a(broadcast, contents, node)
             prev_err = None
             try:
                 if E07_FORMAT.match(callsign_text):
+                    logger.debug("trying e07")
                     return e07(broadcast, contents, node)
             except ValueError as err:
+                logger.debug(F("failed with {}, continuing with normal", err))
                 prev_err = err
             try:
+                logger.debug("trying normal, incl. callsign")
                 return with_callsign(broadcast, contents, node)
             except ValueError as err:
                 if prev_err:
@@ -298,15 +327,50 @@ def fmt_5figure_groups(with_callsign, without_callsign, e07a_format, e07_format)
             return without_callsign(broadcast, contents, node)
     return handle_5figure_format
 
+def dlya_format_handler(format_id):
+    fmt = dbsession.query(priyom.model.Format).get(format_id)
+    simple_handler = simple_format_handler(format_id)
+    def handle_dlya_format(broadcast, contents, node):
+        callsign_node = list(node.findall(pxmlns.Callsign))
+        for node in callsign_node:
+            text = node.text
+            if text is None:
+                continue
+            text = text.lower()
+            if text in {"dyla", "dlya"}:
+                node.getparent().remove(node)
+
+        return simple_handler(broadcast, contents, node)
+    return handle_dlya_format
+
+def fmt_4figure_groups(plain_4figure, with_3figure_callsign):
+    plain_4figure = simple_format_handler(plain_4figure)
+    with_3figure_callsign = simple_format_handler(with_3figure_callsign)
+    def handle_4figure_format(broadcast, contents, node):
+        logger.debug("4fig disambiguator")
+        callsign_node = node.find(pxmlns.Callsign)
+        if callsign_node is not None and callsign_node.text:
+            return with_3figure_callsign(broadcast, contents, node)
+        else:
+            return plain_4figure(broadcast, contents, node)
+
+    return handle_4figure_format
+
+
+_5fig_handler = fmt_5figure_groups(with_callsign=2,
+                                   without_callsign=6,
+                                   e07a_format=8,
+                                   e07_format=9) # 5 figure groups (with callsign)
+
 format_handler_map = {
     1: simple_format_handler(1), # monolyth
-    2: fmt_5figure_groups(with_callsign=2,
-                          without_callsign=6,
-                          e07a_format=8,
-                          e07_format=9), # 5 figure groups (with callsign)
-    3: simple_format_handler(3), # dlya (w/o numbers)
-    4: simple_format_handler(4), # dlya (w/ numbers)
-    # 7: simple_format_handler(5), # empty
+    2: _5fig_handler,
+    3: dlya_format_handler(3), # dlya (w/o numbers)
+    4: dlya_format_handler(4), # dlya (w/ numbers)
+    6: _5fig_handler,
+    8: simple_format_handler(10),
+    9: fmt_4figure_groups(plain_4figure=11,
+                          with_3figure_callsign=12)
 }
 
 ignore_stations = {'196', '197', '198'}
@@ -324,6 +388,23 @@ ignore_transmissions = {
     '411', # horribly broken (fixed in live db)
     '414', # malformed foreign part (fixed in live db)
     '418', # malformed foreign part (fixed in live db)
+    '595', # incorrect “callsign” (fixed in live db)
+    '596', # incorrect data (fixed in live db)
+    '662', # incorrect data (fixed in live db)
+    '664', # incorrect “callsign” (fixed in live db)
+    '738', # bogus callsign (fixed in live db)
+    '806', # messed up, retranscription in pad
+    '1091', # format not recognized by live db, retranscription in pad
+    '1092', # format not recognized by live db, retranscription in pad
+    '1268', # incorrect callsign (fixed in live db)
+    '1286', # incorrect callsign, retranscription in pad
+    '1287', # format not recognized by live db, retranscription in pad
+    '1313', # incorrect callsign (fixed in live db)
+    '1326', # incorrect callsign (fixed in live db)
+    '1355', # incorrect callsign (fixed in live db)
+    '1356', # incorrect callsign (fixed in live db)
+    '1397', # partial log, retranscription in pad
+    '1398', # incorrect callsign (fixed in live db)
 }
 
 override_contents = {
@@ -339,9 +420,16 @@ override_format_handler = {
     '312': simple_format_handler(7) # monolyth with 2 digit groups
 }
 
+callsign_map = {
+    "Al'fa 45": "Al'fa45"
+}
+
 la_ru_callsign_map = {
     "KZJT LNR4": "КЗЙТ ЛНР4",
-    "Al'fa 45": "АЛЬФА 45"
+    "Al'fa45": "АЛЬФА45",
+    "8S1Shch": "8С1Щ",
+    "MDZhB": "МДЖБ",
+    "5J7Shch": "5Й7Щ"
 }
 
 # END OF STATIC DEFINES FOR CONVERSION
@@ -352,6 +440,10 @@ if __name__ == "__main__":
             conversion_state = pickle.load(f)
     except OSError:
         conversion_state = ({}, False, {}, False, {})
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+    )
 
     (station_map, stations_complete,
      broadcast_map, broadcasts_complete,
@@ -365,11 +457,15 @@ if __name__ == "__main__":
             for station_node in tree.find(pxmlns.stations):
                 convert_station(station_node)
             stations_complete = True
+        else:
+            logger.info("skipping station import (state claims to be complete)")
 
         if not broadcasts_complete:
             for broadcast_node in tree.find(pxmlns.broadcasts):
                 convert_broadcast(broadcast_node)
             broadcasts_complete = True
+        else:
+            logger.info("skipping broadcast import (state claims to be complete)")
 
         for transmission_node in tree.find(pxmlns.transmissions):
             convert_transmission(transmission_node)
